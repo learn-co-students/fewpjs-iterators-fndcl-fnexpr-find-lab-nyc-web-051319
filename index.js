23f96b9a38b42b6c5a1f86f50e97ca595ce2306d
njmachin <nicholasjohnmachin@gmail.com>
2019-06-24 16:15:18 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arg) {
+  let obj = arg.find((i,o) => i.result === "W")
+    if (obj === undefined) {
+      return undefined
+    } else {
+    return obj.year
+    }
+  }
