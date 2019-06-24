2aea04137d32524028c2acac41dad8a513a3f7ce
chrisyuaners <chrisyuaners@gmail.com>
2019-06-24 16:45:34 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  if (record.find(function(r) { return r.result === 'W' })) {
+    return record.find(function(r) { return r.result === 'W' }).year
+  }
+  else {
+    return undefined
+  }
+}
