6d10dd7271eb4dee7d0af7aa70a36581297b423f
Will Bainton <baintonw@gmail.com>
2019-06-24 17:27:19 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array) {
+    if (array.find(function(f) {return f.result === "W";})) {
+      return (array.find(function(f) {return f.result === "W";})).year
+    }
+    else {
+      undefined
+    }
+}
+
+// let year = record.find(function(f) {
+// 	return f.result === "W"
+// }
+// ).year
