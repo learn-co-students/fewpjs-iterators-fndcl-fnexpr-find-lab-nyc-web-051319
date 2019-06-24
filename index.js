7649ef4c19b7a1b3d619ffa6806c4dc0d827430a
Aniket <aniketupatel1@gmail.com>
2019-06-24 17:34:25 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records) {
+  let season = records.find(record => record.result === "W")
+  if (season) {
+
+    return season.year
+  }
+}
